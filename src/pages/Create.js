96d50489883dcd8408e4641e2b54a5fba7874be7
Charlie <huh4142@naver.com>
2021-04-05 14:25:04 +1000

    Typography & Button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Typography, Button, ButtonGroup, Container } from '@material-ui/core';
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        variant='h6'
+        component='h2'
+        color='textSecondary'
+        gutterBottom
+      >
+        Create a new Note
+      </Typography>
+
+      <Button type='submit' color='secondary' variant='contained' >Submit</Button>
+
+      {/* <Button variant='contained' color='primary' type='submit'>Submit</Button>
+      <Button variant='outlined' color='secondary' type='submit'>Submit</Button>
+
+      <ButtonGroup color='secondary' variant='contained'>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   )
 }
